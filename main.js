5119af891ea8cb3750616dd29126e2f34b0ef896
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-17 19:44:06 -0600

    Add some retry logic

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
-import { fromEvent } from 'rxjs';
-import { delay, filter, map } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { delay, flatMap, retryWhen, scan, takeWhile } from 'rxjs/operators';
 
-const circle = document.getElementById('circle');
+const outputDiv = document.getElementById('output');
+const button = document.getElementById('button');
 
-let source = fromEvent(document, 'mousemove')
-  .pipe(
-    map(
-      event => ( {
-        x: event.clientX,
-        y: event.clientY
-      } )
-    ),
-    filter(
-      event => event.x < 500
-    ),
-    delay(300)
+let click = fromEvent(button, 'click');
+
+function load(url) {
+  const xhr = new XMLHttpRequest();
+
+  return Observable.create(observer => {
+    xhr.addEventListener('load', () => {
+
+      if (xhr.status >= 200 && xhr.status < 300) {
+        observer.next(JSON.parse(xhr.responseText));
+        observer.complete();
+      } else {
+        observer.error(xhr.statusText);
+      }
+    });
+
+    xhr.open('GET', url);
+    xhr.send();
+  }).pipe(
+    retryWhen(retryStrategy({ attempts: 4, delay: 2000 }))
+  );
+
+}
+
+const retryStrategy = ({ attempts = 4, delay: delayTime = 1000 }) => errors =>
+  errors.pipe(
+    scan(acc => acc + 1, 0),
+    takeWhile(acc => acc < attempts),
+    delay(delayTime)
   );
 
 // More simple observer
-source.subscribe(
-  ({ x, y }) => {
-    circle.style.left = x - circle.clientWidth / 2;
-    circle.style.top = y - circle.clientHeight / 2;
+click
+  .pipe(
+    flatMap(() => load('movies.json'))
+  )
+  .subscribe(
+  movies => {
+    movies.forEach(movie => {
+      const div = document.createElement('div');
+      div.innerText = movie.title;
+      outputDiv.appendChild(div);
+    })
   },
   error => console.log(`error ${error}`),
   () => console.log(( 'complete' ))
