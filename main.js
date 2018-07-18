0877c35d17b5abbc02a103444ab427da9a684ca5
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-17 21:34:36 -0600

    Update load to be cancelable

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-import { defer } from 'rxjs/observable/defer';
-import { fromPromise } from 'rxjs/observable/fromPromise';
+import { Observable } from 'rxjs/Observable';
 import { fromEvent } from 'rxjs/observable/fromEvent';
-import { delay, flatMap, retryWhen, scan } from 'rxjs/operators';
+import { delay, retryWhen, scan } from 'rxjs/operators';
 
 const outputDiv = document.getElementById('output');
 const button = document.getElementById('button');
 
 const click = fromEvent(button, 'click');
 
-const loadWithFetch = url => defer(
-  () => fromPromise(
-    fetch(url).then(response => {
-      if (response.ok) {
-        return response.json();
+function load(url) {
+  const xhr = new XMLHttpRequest();
+
+  return Observable.create(observer => {
+
+    const onLoad = () => {
+
+      if (xhr.status >= 200 && xhr.status < 300) {
+        observer.next(JSON.parse(xhr.responseText));
+        observer.complete();
       } else {
-        return Promise.reject(response)
+        observer.error(xhr.statusText);
       }
-    })
-  )
-).pipe(
-  retryWhen(retryStrategy())
-);
+    };
+
+    xhr.addEventListener('load', onLoad);
+
+    xhr.open('GET', url);
+    xhr.send();
+
+    return () => {
+      xhr.removeEventListener('load', onLoad);
+      xhr.abort();
+    }; // This is run when the Observable is unsubscribed from
+  }).pipe(
+    retryWhen(retryStrategy())
+  );
+}
 
 const retryStrategy = ({ attempts = 4, delay: delayTime = 1000 } = {}) => errors =>
   errors.pipe(
@@ -35,19 +49,22 @@ const retryStrategy = ({ attempts = 4, delay: delayTime = 1000 } = {}) => errors
     delay(delayTime)
   );
 
+const loading = load('movies.json').subscribe(() => {});
+loading.unsubscribe(); // Aborts the xhr request
+
 // More simple observer
-click
-  .pipe(
-    flatMap(() => loadWithFetch('moviess.json'))
-  )
-  .subscribe(
-  movies => {
-    movies.forEach(movie => {
-      const div = document.createElement('div');
-      div.innerText = movie.title;
-      outputDiv.appendChild(div);
-    })
-  },
-  error => console.log(`error ${error}`),
-  () => console.log(( 'complete' ))
-);
+// click
+//   .pipe(
+//     flatMap(() => load('movies.json'))
+//   )
+//   .subscribe(
+//   movies => {
+//     movies.forEach(movie => {
+//       const div = document.createElement('div');
+//       div.innerText = movie.title;
+//       outputDiv.appendChild(div);
+//     })
+//   },
+//   error => console.log(`error ${error}`),
+//   () => console.log(( 'complete' ))
+// );
